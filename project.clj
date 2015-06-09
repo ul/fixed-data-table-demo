f05f6669b57bf6ff2052967890898e70c59baa2b
Ruslan Prokopchuk <fer.obbee@gmail.com>
2015-06-09 12:04:12 +0300

    up deps

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject fixed-data-table-demo "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :url "http://example.com/FIXME"
-            :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                           [org.clojure/clojurescript "0.0-3126"]
+            :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                           [org.clojure/clojurescript "0.0-3308"]
                            [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                            [org.omcljs/om "0.8.8"]
-                           [cljsjs/fixed-data-table "0.1.2-0"]]
-            :plugins [[lein-cljsbuild "1.0.5"]]
+                           [cljsjs/fixed-data-table "0.2.0-0"]]
+            :plugins [[lein-cljsbuild "1.0.6"]]
             :source-paths ["src" "target/classes"]
-            :clean-targets ["out/fixed_data_table_demo" "out/fixed_data_table_demo.js"]
+            :clean-targets ["target" "out/fixed_data_table_demo" "out/fixed_data_table_demo.js"]
             :cljsbuild {:builds [{:id           "dev"
                                   :source-paths ["src"]
                                   :compiler     {:main           fixed-data-table-demo.core
